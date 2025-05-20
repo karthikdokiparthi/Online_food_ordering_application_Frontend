7a11f80bf0ee5315e8b02e4af5188f541434d0cf
karthikdokiparthik <dokiparthikarthik@gmail.com>
2025-05-20 15:28:35 +0530

    feat: Implement routing with protected private routes using React Router and Redux auth state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes, Navigate, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import './App.css';
-import UserLogin from './features/users/UserLogin';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import UserRegistration from './features/users/UserRegistration';
 import Home from './pages/Home';
-import Navbar from './components/Navbar';
+import Restaurant from './features/restaurant/Restaurant';
+import Dishes from './features/restaurant/Dishes';
+import UserLogin from './features/users/UserLogin';
+import UserRegistration from './features/users/UserRegistration';
+import MainLayout from './MainLayout';
+import Cart from './features/Cart/Cart';
+
+// Private Route Component
+const PrivateRoute = ({ children }) => {
+  const isAuthenticated = useSelector(state => state.users.isAuthenticated);
+  return isAuthenticated ? children : <Navigate to="/login" />;
+};
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<UserLogin />} />
-        <Route path='register' element={<UserRegistration />} />
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/login" element={<UserLogin />} />
+        <Route path="/register" element={<UserRegistration />} />
+
+        <Route element={<MainLayout />}>
+          <Route path='/home' element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          } />
+          <Route path='/restaurants' element={
+            <PrivateRoute>
+              <Restaurant />
+            </PrivateRoute>
+          } />
+          <Route path='/dishes' element={
+            <PrivateRoute>
+              <Dishes />
+            </PrivateRoute>
+          } />
+          <Route path='/cart' element={
+            <PrivateRoute>
+              <Cart />
+            </PrivateRoute>
+          } />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
