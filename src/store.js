7b0187a8273b9196e7ee948afb61717ab6ae40bc
karthikdokiparthik <dokiparthikarthik@gmail.com>
2025-05-20 15:25:50 +0530

    feat: Add cart and restaurant reducers to Redux store configuration

--- src/store.js
+++ src/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from './features/users/userSlice';
+import restaurantReducer from './features/restaurant/restaurantSlice';
+import cartReducer from './features/Cart/cartSlice';
 
 const store = configureStore({
     reducer: {
         users: userReducer,
+        restaurant: restaurantReducer,
+        cart: cartReducer
     }
 })
 export default store;
