0bc5243ea1f128517952be6349413c5e6f2c05dd
karthikdokiparthik <dokiparthikarthik@gmail.com>
2025-06-19 13:00:53 +0530

    Fix: Removed unused useNavigate to pass ESLint

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes, Navigate, useNavigate } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Home from './pages/Home';
 import Restaurant from './features/restaurant/Restaurant';
